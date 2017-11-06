9c222f2a5526db3b337391f60ec41495017242fa
marco.rodrigues <mrodrigues@inegi.up.pt>
2017-11-06 17:59:00 +0000

    questionario e versao final

---
+++ codigo_versao_final/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+	//var socket = io.connect('http://localhost:3000');
+
+});
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
