3f72bebb54b6030190e30298335fc1148db4aafa
marco.rodrigues <mrodrigues@inegi.up.pt>
2017-11-08 17:52:26 +0000

    codigfo

--- codigo_versao_final/js/main.js
+++ codigo_versao_final/js/main.js
@@ -1,10 +1,45 @@
 $(function(){
 	//var socket = io.connect('http://localhost:3000');
 
+	var modal = document.getElementById('modal-teclado');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("img-teclado");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on the button, open the modal 
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+
+	$('#btn-teclado-x').on('click',function(){
+		$('#span-mdi-gcode').append('X');
+	});
+	$('#btn-teclado-y').on('click',function(){
+		$('#span-mdi-gcode').append('Y');
+	});
+	$('#btn-teclado-z').on('click',function(){
+		$('#span-mdi-gcode').append('Z');
+	});
+
 });
 
 
-function openCity(evt, cityName) {
+function abreModoOperacao(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -17,3 +52,19 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function abreParametros(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks2");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
